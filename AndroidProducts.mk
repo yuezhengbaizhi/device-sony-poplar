ccce91e0ea6aecb1abeec835e7c0dbbec81c3564
乐正白芷 <42693943+yuezhengbaizhi@users.noreply.github.com>
2019-08-18 00:47:21 +0800

    RR Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/aosp_g8341.mk \
-                     $(LOCAL_DIR)/aosp_g8342.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/rr_g8341.mk \
+                     $(LOCAL_DIR)/rr_g8342.mk
 
