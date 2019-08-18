37da21b6a3b53f11f160386a7d574579a2920fa4
乐正白芷 <42693943+yuezhengbaizhi@users.noreply.github.com>
2019-08-18 13:10:34 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -12,6 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/rr_g8341.mk \
-                     $(LOCAL_DIR)/rr_g8342.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/rr_poplar.mk
 
