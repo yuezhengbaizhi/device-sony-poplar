15d2d22bcc41786577e3c43772bbd51ff5266dd0
乐正白芷 <42693943+yuezhengbaizhi@users.noreply.github.com>
2020-02-23 03:41:25 -0500

    infinity bringup

--- infinity_poplar.mk
+++ infinity_poplar.mk
@@ -26,9 +26,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit Hentai os vendor 
-$(call inherit-product, vendor/hentai/config/common_telephony.mk)
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
-PRODUCT_NAME := hentai_poplar
+PRODUCT_NAME := infinity_poplar
 PRODUCT_DEVICE := poplar
 PRODUCT_MODEL := Xperia XZ1 Dual
 PRODUCT_BRAND := Sony
