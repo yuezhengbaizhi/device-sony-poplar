20c603165bd274d0fd71a85873e2f6a2e1b6e8a4
乐正白芷 <42693943+yuezhengbaizhi@users.noreply.github.com>
2020-02-23 23:39:35 -0500

    Create hentai_poplar_go.mk

---
+++ hentai_poplar_go.mk
@@ -0,0 +1,38 @@
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+TARGET_KERNEL_CONFIG := aosp_yoshino_poplar_defconfig
+
+# DualSim
+PRODUCT_DEVICE_DS := true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.telephony.default_network=9,1
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/sony/poplar/device.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit Hentai os vendor 
+$(call inherit-product, vendor/hentai/config/common_telephony.mk)
+
+# Inherit common Android Go configurations
+$(call inherit-product, build/target/product/go_defaults.mk)
+
+PRODUCT_NAME := hentai_poplar_go
+PRODUCT_DEVICE := poplar
+PRODUCT_MODEL := Xperia XZ1 Dual
+PRODUCT_BRAND := Sony
+PRODUCT_MANUFACTURER := Sony
